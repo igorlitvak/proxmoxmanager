6393780f535cf0cbca52859f4a635e47defb4cba
igorlitvak <igr.litvak@gmail.com>
2021-07-03 21:51:48 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Project name (e. g. pip install proxmoxmanager)
+    name="proxmoxmanager",
+
+    # Current version
+    version="0.0.1",
+
+    # Short description
+    description="Smart Proxmox VE API wrapper for automatically managing resources",
+
+    # Long description
+    long_description="",  # TODO: add full description
+
+    # Link to project's GitHub page
+    url="https://github.com/igorlitvak/proxmoxmanager",
+
+    # Project's author
+    author="Igor Litvak, ITMO University",
+
+    # Which packages are included
+    packages=find_packages(where="proxmoxmanager"),
+
+    # Which Python version is required to run this project
+    python_requires=">=3.8, <4",  # TODO: test on earlier versions
+
+    # Which external projects this project depends on
+    install_requires=["proxmoxer"],
+)
