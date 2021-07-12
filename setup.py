f871921f0bc719bcbd9e62564ea68f747c9d0750
igorlitvak <igr.litvak@gmail.com>
2021-07-12 14:40:04 +0300

    Prepare package to be published on PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    REQUIREMENTS = f.read().splitlines()
+    requirements = f.read().splitlines()
+
+long_description_filename = "README.md"
+try:
+    with open(long_description_filename, "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = f"File {long_description_filename} not found"
+except Exception:
+    long_description = f"Failed to read {long_description_filename} because of unexpected error"
 
 setup(
     # Project name (e. g. pip install proxmoxmanager)
@@ -14,13 +23,19 @@ setup(
     description="Smart Proxmox VE API wrapper for automatically managing resources",
 
     # Long description
-    long_description="",  # TODO: add full description
+    long_description=long_description,
+
+    # Long desctiption content type
+    long_description_content_type="text/markdown",
 
     # Link to project's GitHub page
     url="https://github.com/igorlitvak/proxmoxmanager",
 
     # Project's author
     author="Igor Litvak, ITMO University",
+
+    # Which directory contains source code
+    package_dir={"": "proxmoxmanager"},
 
     # Which packages are included
     packages=find_packages(where="proxmoxmanager"),
@@ -29,5 +44,12 @@ setup(
     python_requires=">=3.8, <4",  # TODO: test on earlier versions
 
     # Which external projects this project depends on
-    install_requires=REQUIREMENTS,
+    install_requires=requirements,
+
+    # Metadata
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
 )
