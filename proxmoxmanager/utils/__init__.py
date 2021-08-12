f4c963c081d5e899ed6c73a304f7d50e394646fb
igorlitvak <igr.litvak@gmail.com>
2021-08-13 00:18:35 +0300

    Change imports

--- proxmoxmanager/utils/__init__.py
+++ proxmoxmanager/utils/__init__.py
@@ -1,3 +1,4 @@
 from .decorators import return_default_on_exception, reraise_exception_on_exception
 from .api import APIWrapper
-from .classes import *
+from .classes import ProxmoxException, ProxmoxNode, ProxmoxNodeList, ProxmoxUser, ProxmoxUserList, ProxmoxVM, \
+    ProxmoxVMList, ProxmoxContainer, ProxmoxContainerList
