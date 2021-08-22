dffab757b47e5681561e5e82cd3e8de9dc6f0266
igorlitvak <igr.litvak@gmail.com>
2021-08-22 19:44:56 +0300

    Tidy up main class

--- proxmoxmanager/main.py
+++ proxmoxmanager/main.py
@@ -1,9 +1,4 @@
-from proxmoxer import ProxmoxAPI
-import re
-import logging
-from typing import Tuple
-
-from proxmoxmanager.utils import *
+from proxmoxmanager.utils import APIWrapper, ProxmoxNodeDict, ProxmoxUserDict, ProxmoxVMDict, ProxmoxContainerDict
 
 
 class ProxmoxManager:
@@ -16,16 +11,32 @@ class ProxmoxManager:
 
     @property
     def nodes(self):
+        """
+        Get all nodes
+        :return: Dict-like object containing nodes
+        """
         return ProxmoxNodeDict(self._api)
 
     @property
     def users(self) -> ProxmoxUserDict:
+        """
+        Get all users
+        :return: Dict-like object containing users
+        """
         return ProxmoxUserDict(self._api)
 
     @property
     def vms(self) -> ProxmoxVMDict:
+        """
+        Get all virtual machines
+        :return: Dict-like object containing virtual machines
+        """
         return ProxmoxVMDict(self._api)
 
     @property
     def containers(self) -> ProxmoxContainerDict:
+        """
+        Get all containers
+        :return: Dict-like object containing containers
+        """
         return ProxmoxContainerDict(self._api)
